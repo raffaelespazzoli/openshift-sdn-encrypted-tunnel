0245803478fba272ef42322fe9d1dbe61a7ce7b1
Raffaele Spazzoli <rspazzol@redhat.com>
2018-10-11 19:01:56 -0700

    added build for coredns container

--- build_push.sh
+++ build_push.sh
@@ -3,3 +3,5 @@
 set -o errexit
 docker build -t quay.io/raffaelespazzoli/openshift-sdn-tunnel:latest .
 docker push quay.io/raffaelespazzoli/openshift-sdn-tunnel:latest
+docker build -t quay.io/raffaelespazzoli/coredns:latest -f Dockerfile.coredns .
+docker push quay.io/raffaelespazzoli/coredns:latest
