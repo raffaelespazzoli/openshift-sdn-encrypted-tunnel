f402e44984aa5c0bafd589461a707e093e1d4168
Raffaele Spazzoli <rspazzol@redhat.com>
2018-10-25 12:50:52 -0400

    updated image

--- build_push.sh
+++ build_push.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -o errexit
-docker build -t quay.io/raffaelespazzoli/openshift-sdn-tunnel:kube-router .
-docker push quay.io/raffaelespazzoli/openshift-sdn-tunnel:kube-router
+docker build -t quay.io/raffaelespazzoli/openshift-sdn-tunnel:latest .
+docker push quay.io/raffaelespazzoli/openshift-sdn-tunnel:latest
 docker build -t quay.io/raffaelespazzoli/kube-router:latest -f Dockerfile.kube-router .
 docker push quay.io/raffaelespazzoli/kube-router:latest
 docker build -t quay.io/raffaelespazzoli/coredns:latest -f Dockerfile.coredns .
